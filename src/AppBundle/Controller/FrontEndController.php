fdcee6c2a7d0fba935ddec94af54bbce15e7258d
ihorkruchynenko <ihorkruchynenko@10.0.0.7>
2016-08-17 21:05:43 +0200

    Great commit

--- src/AppBundle/Controller/FrontEndController.php
+++ src/AppBundle/Controller/FrontEndController.php
@@ -56,7 +56,7 @@ class FrontEndController extends Controller
                 }
                 $hash = $this->mc_encrypt($newSubscriber->getEmailAddress(), $this->generateKey(16));
                 
-                $em = $this->getDoctrine()->getManager();
+                $em = $this->getDoctrine()->getManager('custom');
                 $entity = $em->getRepository('AppBundle:SubscriberDetails') ->findOneBy(['emailaddress' => $emailaddress]);
                 
                 if(!$entity) {
@@ -172,7 +172,7 @@ class FrontEndController extends Controller
         $newOptInDetails = new SubscriberOptInDetails();
         $subscriber = new SubscriberDetails();
         
-        $em = $this->getDoctrine()->getManager();
+        $em = $this->getDoctrine()->getManager('custom');
         $subscriber = $em->getRepository('AppBundle:SubscriberDetails') ->findOneBy(['emailaddress' => $emailaddress]);
         $userid = $subscriber ->getId();
 
@@ -244,7 +244,7 @@ class FrontEndController extends Controller
      */
     public function verifyUnsubscribeAction(Request $request, $emailaddress) {
         $newOptOutDetails = new SubscriberOptOutDetails();
-        $em = $this->getDoctrine()->getManager();
+        $em = $this->getDoctrine()->getManager('custom');
         $subscriber = $em->getRepository('AppBundle:SubscriberDetails') ->findOneBy(['emailaddress' => $emailaddress]);
         
         if(!$subscriber) {
@@ -276,7 +276,7 @@ class FrontEndController extends Controller
         $form->handleRequest($request);
         
         if($form->isValid() && $form->isSubmitted()) {
-            $em = $this->getDoctrine()->getManager();
+            $em = $this->getDoctrine()->getManager('custom');
             $subscriber = $em->getRepository('AppBundle:SubscriberDetails')->findOneByEmailaddress($unsubscriber->getEmailaddress());
 
             if($subscriber) {
